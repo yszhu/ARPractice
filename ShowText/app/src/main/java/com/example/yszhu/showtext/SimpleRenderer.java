515b90f8cb95d0e6f1bca1a2a90d5a4329025526
yszhu <2033503425@qq.com>
2016-07-26 16:52:25 +0800

    pictures can used as texture,if take the place of the SimpleRenderer with TextRenderer,text can also be drew

---
+++ ShowText/app/src/main/java/com/example/yszhu/showtext/SimpleRenderer.java
@@ -0,0 +1,155 @@
+/*
+ *  SimpleRenderer.java
+ *  ARToolKit5
+ *
+ *  Disclaimer: IMPORTANT:  This Daqri software is supplied to you by Daqri
+ *  LLC ("Daqri") in consideration of your agreement to the following
+ *  terms, and your use, installation, modification or redistribution of
+ *  this Daqri software constitutes acceptance of these terms.  If you do
+ *  not agree with these terms, please do not use, install, modify or
+ *  redistribute this Daqri software.
+ *
+ *  In consideration of your agreement to abide by the following terms, and
+ *  subject to these terms, Daqri grants you a personal, non-exclusive
+ *  license, under Daqri's copyrights in this original Daqri software (the
+ *  "Daqri Software"), to use, reproduce, modify and redistribute the Daqri
+ *  Software, with or without modifications, in source and/or binary forms;
+ *  provided that if you redistribute the Daqri Software in its entirety and
+ *  without modifications, you must retain this notice and the following
+ *  text and disclaimers in all such redistributions of the Daqri Software.
+ *  Neither the name, trademarks, service marks or logos of Daqri LLC may
+ *  be used to endorse or promote products derived from the Daqri Software
+ *  without specific prior written permission from Daqri.  Except as
+ *  expressly stated in this notice, no other rights or licenses, express or
+ *  implied, are granted by Daqri herein, including but not limited to any
+ *  patent rights that may be infringed by your derivative works or by other
+ *  works in which the Daqri Software may be incorporated.
+ *
+ *  The Daqri Software is provided by Daqri on an "AS IS" basis.  DAQRI
+ *  MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ *  THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE, REGARDING THE DAQRI SOFTWARE OR ITS USE AND
+ *  OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+ *
+ *  IN NO EVENT SHALL DAQRI BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
+ *  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+ *  MODIFICATION AND/OR DISTRIBUTION OF THE DAQRI SOFTWARE, HOWEVER CAUSED
+ *  AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+ *  STRICT LIABILITY OR OTHERWISE, EVEN IF DAQRI HAS BEEN ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  Copyright 2015 Daqri, LLC.
+ *  Copyright 2011-2015 ARToolworks, Inc.
+ *
+ *  Author(s): Julian Looser, Philip Lamb
+ *
+ */
+
+package com.example.yszhu.showtext;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLES10;
+import android.opengl.GLUtils;
+
+import org.artoolkit.ar.base.ARToolKit;
+import org.artoolkit.ar.base.rendering.ARRenderer;
+import org.artoolkit.ar.base.rendering.RenderUtils;
+
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.util.BitSet;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * A very simple Renderer that adds a marker and draws a cube on it.
+ */
+public class SimpleRenderer extends ARRenderer {
+
+    private Context context=null;
+    private int markerID = -1;
+    private Rect cube = new Rect(40.0f, 0.0f, 0.0f, 20.0f);
+    private int texture;
+    /**
+     * Markers can be configured here.
+     */
+    SimpleRenderer(Context context){
+        this.context=context;
+    }
+    @Override
+    public boolean configureARScene() {
+
+        markerID = ARToolKit.getInstance().addMarker("single;Data/hiro.patt;80");
+        if (markerID < 0) return false;
+        return true;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        super.onSurfaceCreated(unused, config);
+        unused.glEnable(GL10.GL_TEXTURE_2D);
+        loadTexture(unused);
+    }
+
+    /**
+     * Override the draw function from ARRenderer.
+     */
+    @Override
+    public void draw(GL10 gl) {
+
+
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+        // Apply the ARToolKit projection matrix
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadMatrixf(ARToolKit.getInstance().getProjectionMatrix(), 0);
+
+        gl.glEnable(GL10.GL_CULL_FACE);
+        gl.glShadeModel(GL10.GL_SMOOTH);
+        gl.glEnable(GL10.GL_DEPTH_TEST);
+        gl.glFrontFace(GL10.GL_CW);
+
+        // If the marker is visible, apply its transformation, and draw a cube
+        if (ARToolKit.getInstance().queryMarkerVisible(markerID)) {
+            gl.glMatrixMode(GL10.GL_MODELVIEW);
+            gl.glLoadMatrixf(ARToolKit.getInstance().queryMarkerTransformation(markerID), 0);
+            /*gl.glEnableClientState(GLES10.GL_COLOR_ARRAY);
+            gl.glEnableClientState(GLES10.GL_VERTEX_ARRAY);*/
+            cube.draw(gl,texture);
+
+        }
+
+    }
+    private void loadTexture(GL10 gl){
+        Bitmap bitmap=null;
+        try{
+            bitmap= BitmapFactory.decodeResource(context.getResources(),R.drawable.sand);
+            /*声明存储所生成纹理编号的数组，这里只生成一个纹理*/
+            int []textures=new int[1];
+            /*指定生成纹理个数并将编号存入数组,第一个参数指定生成多少个纹理，第二个参数是存放纹理编号的数组*/
+            gl.glGenTextures(1,textures,0);
+            /*获取第一个纹理*/
+            texture=textures[0];
+            /*将texture代表的纹理绑定到GL_TEXTURE_2D目标中*/
+            gl.glBindTexture(GL10.GL_TEXTURE_2D,texture);
+            /*设置纹理缩小（距离视点远时被缩小）时的滤波方式*/
+            gl.glTexParameterf(GL10.GL_TEXTURE_2D,GL10.GL_TEXTURE_MIN_FILTER,GL10.GL_NEAREST);
+            /*放大时*/
+            gl.glTexParameterf(GL10.GL_TEXTURE_2D,GL10.GL_TEXTURE_MAG_FILTER,GL10.GL_LINEAR);
+            /*设置横向、纵向上都平铺纹理*/
+            gl.glTexParameterf(GL10.GL_TEXTURE_2D,GL10.GL_TEXTURE_WRAP_S,GL10.GL_REPEAT);
+            gl.glTexParameterf(GL10.GL_TEXTURE_2D,GL10.GL_TEXTURE_WRAP_T,GL10.GL_REPEAT);
+            /*加载Bitmap生成纹理*/
+            GLUtils.texImage2D(GL10.GL_TEXTURE_2D,0,bitmap,0);
+        }
+        finally {
+
+        }
+    }
+
+}
